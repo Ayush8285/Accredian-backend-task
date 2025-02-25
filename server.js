7d54b49d960b8f04315c98eab573e2da2b19e9ad
Ayush8285 <113717319+Ayush8285@users.noreply.github.com>
2025-02-25 14:02:55 +0530

    content

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(cors({ origin: "*" })); // Allow all domains
 
 app.use(bodyParser.json());
 
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
 app.use("/api", referralRoutes);
 
 app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
